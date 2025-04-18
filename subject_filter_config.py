76ddf338c45eb8d9b9a9afd221ef6d02e05a8720
MAPULVER1 <michaelalexanderpulver@outlook.com>
2025-04-18 18:03:34 -0400

    Create subject_filter_config.py

---
+++ subject_filter_config.py
@@ -0,0 +1,13 @@
+SUBJECT_OPTIONS = [
+    "The Executive Branch",
+    "The Legislative Branch",
+    "The Judicial Branch",
+    "Education",
+    "Technology",
+    "Business & the Economy",
+    "World Leaders",
+    "International Conflicts",
+    "Business & Commerce",
+    "The Global Economy",
+    "Human Rights"
+]
