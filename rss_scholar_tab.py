8a74d4c66bbe979492516829e57b2b288576e930
MAPULVER1 <michaelalexanderpulver@outlook.com>
2025-04-18 18:53:42 -0400

    Update rss_scholar_tab.py

--- rss_scholar_tab.py
+++ rss_scholar_tab.py
@@ -7,10 +7,26 @@ from datetime import datetime
 def rss_scholar_tab(username):
     st.markdown("### 📡 Pull a Live Article to Log")
     rss_sources = {
-        "NPR": "https://www.npr.org/rss/rss.php?id=1001",
-        "Reuters": "http://feeds.reuters.com/reuters/topNews",
-        "BBC": "http://feeds.bbci.co.uk/news/rss.xml",
-        "Al Jazeera": "https://www.aljazeera.com/xml/rss/all.xml"
+            "NPR": "https://www.npr.org/rss/rss.php?id=1001",
+            "Reuters": "http://feeds.reuters.com/reuters/topNews",
+            "BBC News": "http://feeds.bbci.co.uk/news/rss.xml",
+            "CNN": "http://rss.cnn.com/rss/edition.rss",
+            "The Guardian": "https://www.theguardian.com/world/rss",
+            "Al Jazeera": "https://www.aljazeera.com/xml/rss/all.xml",
+            "Fox News": "http://feeds.foxnews.com/foxnews/latest",
+            "Bloomberg": "https://www.bloomberg.com/feed/podcast/etf-report.xml",
+            "The Washington Post": "http://feeds.washingtonpost.com/rss/national",
+            "ABC News": "https://abcnews.go.com/abcnews/topstories",
+            "CBS News": "https://www.cbsnews.com/latest/rss/main",
+            "NBC News": "http://feeds.nbcnews.com/nbcnews/public/news",
+            "USA Today": "http://rssfeeds.usatoday.com/usatoday-NewsTopStories",
+            "Politico": "https://www.politico.com/rss/politics08.xml",
+            "The Hill": "https://thehill.com/rss/syndicator/19110",
+            "Time": "http://feeds2.feedburner.com/time/topstories",
+            "Newsweek": "https://www.newsweek.com/rss",
+            "The Atlantic": "https://www.theatlantic.com/feed/all/",
+            "Sky News": "https://feeds.skynews.com/feeds/rss/home.xml",
+            "Axios": "https://www.axios.com/rss"
     }
 
     selected_source = st.selectbox("Choose a Source", list(rss_sources.keys()))
