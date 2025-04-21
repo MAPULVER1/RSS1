a173ecb00e2a6cc5528452ce6ba0e89c484725fe
MAPULVER1 <michaelalexanderpulver@outlook.com>
2025-04-21 18:35:13 -0400

    Update peer_question_tab.py

--- peer_question_tab.py
+++ peer_question_tab.py
@@ -3,9 +3,14 @@ import pandas as pd
 import subprocess
 import streamlit as st
 import os
+from data_loader import load_scholar_logs, get_user_logs, get_summary
 
 if "GITHUB_TOKEN" in st.secrets:
     os.environ["GITHUB_TOKEN"] = st.secrets["GITHUB_TOKEN"]
+
+df = load_scholar_logs()
+user_df = get_user_logs("gabe", df)
+summary_df = get_summary(df)
 
 def auto_git_push():
     try:
